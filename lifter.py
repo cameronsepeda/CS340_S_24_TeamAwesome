19d2ec3d0381473975b48c0f35d1343b694b742b
cameronsepeda <98052244+cameronsepeda@users.noreply.github.com>
2024-03-30 17:12:11 -0500

    Create lifter.py

---
+++ lifter.py
@@ -0,0 +1,98 @@
+#%% MODULE BEGINS
+module_name = 'lifter'
+
+'''
+Version: <***>
+
+Description:
+    <***>
+
+Authors:
+    <***>
+
+Date Created     :  <***>
+Date Last Updated:  <***>
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+   import os
+   #os.chdir("./../..")
+#
+
+#custom imports
+import Person from person.py
+
+#other imports
+from   copy       import deepcopy as dpcpy
+
+'''
+from   matplotlib import pyplot as plt
+import mne
+import numpy  as np 
+import os
+import pandas as pd
+import seaborn as sns
+'''
+#%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+class Lifter implements Person:
+  bench = '0'
+  squat = '0'
+  deadlift = '0'
+  
+  def __init__(self, lBench, lSquat, lDeadlift):
+    pass
+
+  def showViolin(self):
+    pass
+
+  def showWhisker(self):
+    pass
+
+  def showScatter(self):
+    pass
+
+
+#Function definitions Start Here
+def main():
+    pass
+#
+
+#%% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main code start here
+
+
+
+#%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main Self-run block
+if __name__ == "__main__":
+    
+    print(f"\"{module_name}\" module begins.")
+    
+    #TEST Code
+    main()
